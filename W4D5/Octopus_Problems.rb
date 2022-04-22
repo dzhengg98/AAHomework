67d7cde38febdc6085ea3560a62e88886e1cf4ea
dzheng <dzheng3456@gmail.com>
2022-04-21 21:26:22 -0400

    changed constant dance

--- W4D5/Octopus_Problems.rb
+++ W4D5/Octopus_Problems.rb
@@ -24,17 +24,18 @@ def clever_octopus(arr)
 end
 p clever_octopus(['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'])
 #----------Dancing Octopus----------
-tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
 # Slow Dance O(n)
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
 def slow_dance(str, arr)
   arr.each_with_index {|ele, i| return i if str == ele}
 end
 p slow_dance("up", tiles_array)
 p slow_dance("right-down", tiles_array)
+
 # Constant Dance! O(1)
-def fast_dance(str, arr)
-  hash = Hash[arr.map.with_index.to_a]
+tiles_hash = {"up"=>0, "right-up"=>1, "right"=>2, "right-down"=>3, "down"=>4, "left-down"=>5, "left"=>6, "left-up"=>7}
+def fast_dance(str, hash)
   hash[str]
 end
-p fast_dance("up", tiles_array)
-p fast_dance("right-down", tiles_array)
+p fast_dance("up", tiles_hash)
+p fast_dance("right-down", tiles_hash)
