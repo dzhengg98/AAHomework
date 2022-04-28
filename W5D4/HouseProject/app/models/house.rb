81177682f83d24f53ba92d3e85a980f7faf86a83
dzheng <dzheng3456@gmail.com>
2022-04-28 08:35:52 -0400

    finished hw

---
+++ W5D4/HouseProject/app/models/house.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id         :bigint           not null, primary key
+#  address    :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class House < ApplicationRecord
+  has_many(:people, {
+    primary_key: :id,
+    foreign_key: :house_id, 
+    class_name: :Person
+  })
+end
