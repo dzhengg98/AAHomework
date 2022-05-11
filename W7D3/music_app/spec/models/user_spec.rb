99ce38c2dcc4aa8d3ad5b6678644a14beadbd900
David Zheng <dzheng3456@gmail.com>
2022-05-11 06:36:25 -0400

    made the set ups for controllers and features

--- W7D3/music_app/spec/models/user_spec.rb
+++ W7D3/music_app/spec/models/user_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe User do
+    subject(:user) {User.new(email: "vh71886@aa.com", password: "vh71886")}
 
-describe User do
-  it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest)}
+    it { should validate_length_of(:password).is_at_least(6).allow_nil}
+
+    describe "#is_password?"
+
+    describe "#reset_session_token"
+    
+    describe "::find_by_credentials"
+  end
 end
