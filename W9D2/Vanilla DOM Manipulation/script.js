3e5bb1db0d2c418dc97032c6b86caab5fb3eea98
David Zheng <dzheng3456@gmail.com>
2022-05-24 07:37:35 -0400

    phase 3 finished

--- W9D2/Vanilla DOM Manipulation/script.js
+++ W9D2/Vanilla DOM Manipulation/script.js
@@ -41,6 +41,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- your code here!
 
+  const photoButtonToggle = document.querySelector(".photo-show-button");
+  const photoForm = document.querySelector(".photo-form-container");
 
+  // makes the click of the button toggle the form from hidden to shown
+  photoButtonToggle.addEventListener('click', () => {
+    photoForm.classList.toggle('hidden');
+  });
+
+  // adds a submit button to the event, takes the src and shows the image
+  photoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const photoInput = document.querySelector('.photo-url-input');
+    const img = document.createElement('img');
+
+    img.src = photoInput.value;
+    const li = document.createElement('li');
+
+    li.appendChild(img);
+
+    const photosUl = document.querySelector(".dog-photos");
+    photosUl.appendChild(li);
+  })
 
 });
